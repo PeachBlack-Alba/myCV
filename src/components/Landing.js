7fc8df3096f6c76509179c906498c29a7fe4b1e4
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-02 10:56:52 +0200

    commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import Footer from "../components/Footer";
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
+        <h1>Say something</h1>
         <Footer></Footer>
       </div>
     );
