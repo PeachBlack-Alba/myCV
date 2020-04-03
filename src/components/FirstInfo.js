be2da33c314c049a5691567a183b0e8fb7dd18fb
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-03 13:45:33 +0200

    commit

---
+++ src/components/FirstInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+
+import React from "react";
+
+export default function FirstInfo() {
+  return (
+    <Container>
+      <div className="aboutMe"></div>
+    </Container>
+  );
+}
