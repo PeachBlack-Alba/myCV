4644bf31a584ce7551a46bff2a49853e7e94eed8
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-09 16:50:45 +0200

    trying new layout

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,9 +9,81 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>Say something</h1>
-        <FirstInfo></FirstInfo>
-        <Footer></Footer>
+        <div className="home-sky">
+          <div className="home-channel home-channel--first">
+            <h2 className="home-channel__title">
+              <a href="">Courses</a>
+            </h2>
+          </div>
+          <div className="home-channel home-channel--second">
+            <h2 class="home-channel__title">
+              <a href="">Posts</a>
+            </h2>
+          </div>
+          <div className="home-channel home-channel--third">
+            <h2 class="home-channel__title">
+              <a href="">Talks</a>
+            </h2>
+          </div>
+          <div className="home-channel home-channel--fourth">
+            <h2 class="home-channel__title">
+              <a href="">Reading</a>
+            </h2>
+          </div>
+          <div class="home-logo">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="home-grid">
+          {/* <div class="home-grid">
+            <div class="grid" aria-hidden="true">
+              <div class="grid__horizon"></div>
+              <div class="grid__deck">
+                <div class="grid__grid-plane">
+                  <div class="grid__grid">
+                    <div class="grid__grid--vertical">
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                    </div>
+                    <div class="grid__dots">
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                      <div class="grid__dots__dot"></div>
+                    </div>
+                    <div class="grid__grid--horizontal">
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                      <div class="grid__grid__line"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </div>
+
+        {/* <FirstInfo></FirstInfo>
+        <Footer></Footer> */}
       </div>
     );
   }
