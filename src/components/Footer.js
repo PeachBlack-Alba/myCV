71e87e79e52f4b4393a8bc1279ab4054646a28e1
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-16 10:41:35 +0200

    cosas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import windowsflag from "../media/Computer_icons/windowsflag.ico";
-import homebutton from "../media/Computer_icons";
+import homebutton from "../media/Computer_icons/carpeta.ico";
+import nosound from "../media/Computer_icons/nosound.png";
 
 export default function Footer() {
   return (
@@ -11,11 +12,13 @@ export default function Footer() {
           <div className="start">Start</div>
         </div>
         <div className="homeButton">
-          <div>Home</div>
+          <img src={homebutton} alt="carpetaicon" />
+
+          <div className="home">Home</div>
         </div>
       </div>
       <div className="soundTimeWrapper">
-        <img></img>
+        <img src={nosound} alt="nosound" />
         <div className="watch">14:50</div>
       </div>
     </div>
