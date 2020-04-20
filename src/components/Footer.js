f8ffa2d1c30f7d55633f3cd95b3f1ae8ec9a1094
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-20 15:25:26 +0200

    clock done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import windowsflag from "../media/Computer_icons/windowsflag.ico";
 import homebutton from "../media/Computer_icons/carpeta.ico";
 import nosound from "../media/Computer_icons/nosound.png";
-import Clock from "../components/Clock";
 
-export default function Footer() {
-  return (
-    <div className="navBarWrapper">
-      <div className="startHomeWrapper">
-        <div className="startButton">
-          <img src={windowsflag} alt="windowsflag" />
-          <div className="start">Start</div>
+export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString(),
+    });
+  }
+  render() {
+    return (
+      <div className="navBarWrapper">
+        <div className="startHomeWrapper">
+          <div className="startButton">
+            <img src={windowsflag} alt="windowsflag" />
+            <div className="start">Start</div>
+          </div>
+          <div className="homeButton">
+            <img src={homebutton} alt="carpetaicon" />
+
+            <div className="home">Home</div>
+          </div>
         </div>
-        <div className="homeButton">
-          <img src={homebutton} alt="carpetaicon" />
-
-          <div className="home">Home</div>
+        <div className="soundTimeWrapper">
+          <img src={nosound} alt="nosound" />
+          {this.state.time}
         </div>
       </div>
-      <div className="soundTimeWrapper">
-        <img src={nosound} alt="nosound" />
-        {/* <div className="watch">14:50</div> */}
-        <Clock className="watch"></Clock>
-      </div>
-    </div>
-  );
+    );
+  }
 }
