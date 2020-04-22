8c4a9da087290cab3e155915b140d107445a6f82
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-04-22 17:20:17 +0200

    window

---
+++ src/components/AboutMe.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <div className="window">
+          <div className="wrapperClose">
+            <div className="imgFolder">
+              <img></img>
+            </div>
+            <div ClassName="windowTittle"></div>
+          </div>
+          <div className="iconos">
+            <img></img>
+            <img></img>
+            <img></img>
+          </div>
+          <div className="content">
+            <img></img>
+          </div>
+          <div className="footerWindow"></div>
+        </div>
+      </div>
+    );
+  }
+}
