c057b5ca4dc798cecc73f4607150a22f808d296d
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-05-11 19:27:57 +0200

    commit

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../../Context/ThemeContext";
+// import React, { useContext } from "react";
+// import { ThemeContext } from "../../Context/ThemeContext";
 
-export default function ScrollDown() {
-  const themecon = useContext(ThemeContext);
-  return (
-    <div className="scrollIcon">
-      {themecon.canScroll === true && (
-        <div className="scroll-downs">
-          <div className="mousey">
-            <div className="scroller"></div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
+// export default function ScrollDown() {
+//   const themecon = useContext(ThemeContext);
+//   return (
+//     <div className="scrollIcon">
+//       {themecon.canScroll === true && (
+//         <div className="scroll-downs">
+//           <div className="mousey">
+//             <div className="scroller"></div>
+//           </div>
+//         </div>
+//       )}s
+//     </div>
+//   );
+// }
